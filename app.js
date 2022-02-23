9b8d19c89a3dbcd461000a591391ceff2f01fc8d
kigundalevi <levimaxkigunda@gmail.com>
2022-02-24 00:23:54 +0300

    ajax testing

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// document.getElementById('dashboard').addEventListener('click',displaydash);
+document.getElementById('expense').addEventListener('click',displayexpense);
           
-// function displaydash(){
-//   var xhr = new XMLHttpRequest();
-// xhr.open('GET','dash.html',true);
-// xhr.onload = function(){
-//    if(this.status === 200){
-//        document.getElementById('content1').innerHTML = this.responseText;
-//    }
-// }
-// xhr.send();
-// }
+function displayexpense(){
+  var xhr = new XMLHttpRequest();
+xhr.open('GET','expense.html',true);
+xhr.onload = function(){
+   if(this.status === 200){
+       document.getElementById('content1').innerHTML = this.responseText;
+   }
+}
+xhr.send();
+}
 let menu_bar = document.getElementById('menubar');
 menu_bar.onclick = function(){
     document.getElementById('side_bar').style.width='250px';
@@ -20,14 +20,14 @@ close_bar.onclick = function(){
     document.getElementById('side_bar').style.width='0';
     document.getElementById('maincontainer').style.marginLeft='0';
 }
-  let list = document.querySelectorAll('.sidebar li');
-  function activelink(){
-      list.forEach((item) =>
-      item.classList.remove('active'));
-      this.classList.add('active');
-  }
-  list.forEach((item) =>
-  item.addEventListener('click',activelink));
+//   let list = document.querySelectorAll('.sidebar li');
+//   function activelink(){
+//       list.forEach((item) =>
+//       item.classList.remove('active'));
+//       this.classList.add('active');
+//   }
+//   list.forEach((item) =>
+//   item.addEventListener('click',activelink));
 
 
 //  document.getElementById('user').addEventListener('click', display);
